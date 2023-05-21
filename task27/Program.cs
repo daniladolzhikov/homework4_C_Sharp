880d65de20ee248db1e5610eb8e7d51b33383ac3
daniladolzhikov <d.dolzhikov17@gmail.com>
2023-05-21 18:24:21 +0200

    add task 27

---
+++ task27/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int CalculateDigitSum(int number)
+{
+    int sum = 0;
+    while (number != 0)
+    {
+        int digit = number % 10;
+        sum += digit;
+        number /= 10;
+    }
+    return sum;
+}
+
+
+System.Console.WriteLine("Введите любое число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sum = CalculateDigitSum(number);
+Console.WriteLine($"Сумма цифр: " + sum);
